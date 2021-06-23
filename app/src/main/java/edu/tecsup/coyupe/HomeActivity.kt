d38b83e0d1571f773f6dc680c1d0f25cd34855ae
Ivan <ivan.antonio@tecsup.edu.pe>
2021-06-23 18:08:09 -0500

    Initial commit

---
+++ app/src/main/java/edu/tecsup/coyupe/HomeActivity.kt
@@ -0,0 +1,110 @@
+package edu.tecsup.coyupe
+
+import android.content.Intent
+import android.os.Binder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.firebase.auth.FirebaseAuth
+import edu.tecsup.coyupe.databinding.ActivityHomeBinding
+import edu.tecsup.coyupe.databinding.ActivityMainBinding
+
+class   HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var user: FirebaseAuth
+    private lateinit var toogle: ActionBarDrawerToggle
+
+//    var ButtonSalir:Button=findViewById(R.id.ButtonSalir)
+//    var NombreId:TextView=findViewById(R.id.NombreId)
+//    var EmailUser:TextView=findViewById(R.id.EmailUsers)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toogle = ActionBarDrawerToggle(this, binding.draweLayout, R.string.open_drawer, R.string.close_drawer)
+        binding.draweLayout.addDrawerListener(toogle)
+
+        toogle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.NavContainer.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home ->   {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainer,HomeFragment() )
+                        commit()
+                    }
+                }
+
+                R.id.nav_contact ->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainer, ContactFragment())
+                        commit()
+                    }
+                }
+
+                R.id.nav_help ->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainer, HelpFragment())
+                        commit()
+                    }
+                }
+
+                R.id.nav_ubication ->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainer, LocationFragment())
+                        commit()
+                    }
+                }
+
+                R.id.nav_carrito ->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainer, ShoppingFragment())
+                        commit()
+                    }
+                }
+
+                R.id.nav_SignUp -> Toast.makeText(this, "Salte D:", Toast.LENGTH_SHORT).show()
+
+            }
+            binding.draweLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+        /** Funcion para cerrar sesion */
+//        user = FirebaseAuth.getInstance()
+//
+//        if (user.currentUser != null){
+//            user.currentUser?.let {
+//
+//                binding.NombreId.text = it.email
+//            }
+//        }
+//        binding.ButtonSalir.setOnClickListener{
+//            user.signOut()
+//            startActivity(
+//                Intent(
+//                    this,
+//                    MainActivity::class.java
+//                )
+//            )
+//            finish()
+//        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toogle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/edu/tecsup/coyupe/MainActivity.kt
@@ -0,0 +1,87 @@
+package edu.tecsup.coyupe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.firebase.auth.FirebaseAuth
+import edu.tecsup.coyupe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var user: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        user = FirebaseAuth.getInstance()
+
+        /** Funcion */
+//      SignCessionFine()
+
+        binding.btnLogin.setOnClickListener{
+            registerUser()
+        }
+
+        binding.ButtoRegister.setOnClickListener{
+            registerUserCoyuPe()
+        }
+    }
+
+    private  fun registerUserCoyuPe(){
+        startActivity(Intent(this, RegistroActivity::class.java))
+    }
+
+/** Funcion para que cuando cierres la aplicacion se mantenga el login del usuario*/
+//    private fun SignCessionFine(){
+//        if (user.currentUser !=null){
+//            startActivity(Intent(this, HomeActivity::class.java))
+//            finish()
+//        }
+//    }
+
+    private fun registerUser(){
+
+        val email = binding.CorreoEdit.text.toString()
+        val password = binding.PassEdit.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty()){
+
+            user.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(MainActivity()) { task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this,
+                            "User added successfully",
+                            Toast.LENGTH_SHORT).show()
+
+                        startActivity(Intent(this, HomeActivity::class.java))
+                        finish()
+
+                    }else{
+                        user.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener {mtask ->
+                                if (mtask.isSuccessful){
+                                    startActivity(Intent(this, HomeActivity::class.java))
+                                    finish()
+                                }else{
+
+                                    Toast.makeText(this, "Tu contraseña es incorrecta", Toast.LENGTH_SHORT).show()
+//                                    Toast.makeText(this,
+//                                        task.exception!!.message,
+//                                        Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                    }
+                }
+        }else{
+            Toast.makeText(this,
+                "El campo correo y contraseña estan vacios",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+}
